3782790370fc71d0e1efa5d49a17f98f1eb8bf91
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-09-17 01:45:20 +0530

    Mature the Ingesion & Transformation logics

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts','train.csv')
@@ -20,7 +22,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the Data Ingestion Component")
         try:
-            df = pd.read_csv(r'C:\Users\Dell\PycharmProjects\nycopendata\notebook\data\stud.csv')
+            df = pd.read_csv('notebook/data/stud.csv')
             logging.info("Read the dataset as Dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -43,4 +45,7 @@ class DataIngestion:
             raise CustomException("Invalid Config", e, sys)
 
 data_ingestion = DataIngestion()
-data_ingestion.initiate_data_ingestion()
+train_data, test_data = data_ingestion.initiate_data_ingestion()
+
+data_transformation = DataTransformation()
+data_transformation.initiate_data_transformation(train_data, test_data)
