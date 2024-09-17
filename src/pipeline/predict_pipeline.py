bbfa6682a3d13f85669ecb341e6a636ce3b57a51
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-09-17 10:18:02 +0530

    Resolve form attrib issue

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 
@@ -8,19 +9,31 @@ class PredictPipeline:
     def __init__(self):
         pass
 
+    def predict(self, features):
+        try:
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scale = preprocessor.transform(features)
+            preds = model.predict(data_scale)
+            return preds
+        except Exception as e:
+            raise CustomException('Invalid Model Artifacts Load', e, sys)
+
 class CustomData:
     def __init__(self, gender: str,
                  race_ethnicity: int,
                  parental_level_of_education,
                  lunch: int,
-                 test_preparaton_course: str,
+                 test_preparation_course: str,
                  reading_score: int,
                  writing_score: int ):
         self.gender = gender
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
-        self.test_preparation_course = test_preparaton_course
+        self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
 
@@ -39,17 +52,3 @@ class CustomData:
             return pd.DataFrame(custom_data_input_dict)
         except Exception as e:
             raise CustomException('Invalid Custom Data Config', e, sys)
-
-    def predict(self, features):
-        try:
-            model_path = "artifacts/model.pkl"
-            preprocessor_path = "artifacts/preprocessor.pkl"
-            model = load_object(file_path=model_path)
-            preprocessor = load_object(file_path=preprocessor_path)
-            data_scale = preprocessor.transform(features)
-            preds = model.predict(data_scale)
-            return preds
-        except Exception as e:
-            raise CustomException('Invalid Model Artifacts Load', e, sys)
-
-
