a9f51b87ad8da6a097bcf2cc60399d4f064df14b
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-10-04 22:39:24 +0530

    Add TF Configurations to create K8s cluster in Azure

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_location" {
+  description = "The Resource Group location should be passed as Environment Variable"
+}
+
+variable "resource_group_name" {
+  description = "The Resource Group name should be passed as Environment Variable"
+}
+
+variable "agent_count" {
+  default = 3
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+  default = "k8sguru"
+}
+
+variable "cluster_name" {
+  default = "k8sguru"
+}
+
+variable "aks_service_principal_app_id" {
+  description = "The Service Principal Client ID should be passed as Environment Variable"
+}
+
+variable "aks_service_principal_client_secret" {
+  description = "The Service Principal Secret should be passed as Environment Variable"
+}
