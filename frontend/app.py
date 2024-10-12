0c2ee5f748d8ca6813f295aaa6fb4af0d3720465
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-10-12 11:23:36 +0530

    Create folder structure for frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,22 +1,17 @@
 from flask import Flask, request, render_template
 import pandas as pd
 import mlflow
-from os import environ
 
-
-application = Flask(__name__,  template_folder='student_performance/frontend')
-
-app = application
-mlflow_tracking_uri = environ["MLFLOW_TRACKING_URI"]
+app = Flask(__name__,  template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('student_performance/home.html')
 
 @app.route('/stud_perf', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
-        return render_template('home.html')
+        return render_template('student_performance/home.html')
     else:
         form_data = {
             "gender": [str(request.form.get("gender"))],
@@ -30,13 +25,12 @@ def predict_datapoint():
 
         form_data_df = pd.DataFrame(form_data)
 
-        mlflow.set_tracking_uri(mlflow_tracking_uri)
         loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/stud_perf_regressor/latest")
 
         # Use the abstract function in FastTextWrapper to fetch the trained model.
         results = loaded_model.predict(form_data_df)
 
-        return render_template("home.html", results=f"Student's Estimated Math Score is {results[0]}")
+        return render_template("student_performance/home.html", results=f"Student's Estimated Math Score is {results[0]}")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
